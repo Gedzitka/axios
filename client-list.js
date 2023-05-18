acf7a0032cba6a0ac9bb92846c1a2b13402fb013
Jana Dvorakova <dvorakova.jana.vf@gmail.com>
2023-05-18 15:19:10 +0200

    get

--- client-list.js
+++ client-list.js
@@ -1,4 +1,60 @@
 
+axios.get(`http://localhost:8000/api/clients/`)
+    
+    .then((res) => {
+        const data = res.data;
+
+
+
+
+    const clientData = document.getElementById("table");
+    let table = ` <thead class="thead-light">
+    <tr>
+      <th scope="col">Jméno</th>
+      <th scope="col">Bydliště</th>
+      <th scope="col"></th>
+
+    </tr>
+  </thead>
+  <tbody id="tbody" class="w-auto"> `;
+    for (let i = 0; i < data.length; i++) {
+      table += `
+      
+      <tr id= "${data[i]._id}">
+      <td data-label="Jméno a příjmeni"><a href="client-detail.html">${data[i].firstName} ${data[i].lastName}</a></td>
+      <td data-label="telefon">${data[i].streetNumber}, ${data[i].posteNumber}, ${data[i].city}</td>
+      <td data-label="button"><button id="deleteBtn" type="button" class="mr-2 btn btn-danger">Odstranit</button><button type="button" class="  btn btn-warning">Editovat</button></td>
+    </tr>
+
+      `
+   
+    }
+    table += `</tbody>
+                </table>`;
+                clientData.innerHTML = table;
+
+
+
+
+    })
+    .catch((err) =>
+        console.log(err));
+
+        
+
+    
+   
+   
+    
+    
+
+
+    
+
+      
+
+    
+ 
 
    
 
